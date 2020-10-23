92e3ecf1032366e4275d8edb3783bfb2a802277d
matthewgalloway <matthew.galloway19@gmail.com>
2020-10-23 19:19:16 +0100

    further setup files

---
+++ config/config.py
@@ -0,0 +1,20 @@
+
+# numerical vals
+NUMERICALS__VARS = ["age", "balance", "day", "duration", "campaign", "pdays", "previous"]
+
+
+
+# categorical variables to encode
+CATEGORICAL_VARS = [
+    "job",
+    "marital",
+    "education",
+    "default",
+    "housing",
+    "loan",
+    "contact",
+    "month",
+    "poutcome",
+]
+
+
