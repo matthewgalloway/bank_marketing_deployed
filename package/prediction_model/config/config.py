91bc3f9f3585b4c57d314254d73bb94ee88ae861
matthewgalloway <matthew.galloway19@gmail.com>
2020-10-25 11:26:27 +0000

    pipeline updates

--- package/prediction_model/config/config.py
+++ package/prediction_model/config/config.py
@@ -1,10 +1,12 @@
 
-# numerical vals
+import pathlib
+
+import package.prediction_model as prediction_model
+
+# numerical variables
 NUMERICALS__VARS = ["age", "balance", "day", "duration", "campaign", "pdays", "previous"]
 
-
-
-# categorical variables to encode
+# categorical variables
 CATEGORICAL_VARS = [
     "job",
     "marital",
@@ -18,3 +20,30 @@ CATEGORICAL_VARS = [
 ]
 
 
+TRAIN_DATASET_DIR = 'package/prediction_model/datasets/train.csv'
+
+FEATURES = ['age',
+             'job',
+             'marital',
+             'education',
+             'default',
+             'balance',
+             'housing',
+             'loan',
+             'contact',
+             'day',
+             'month',
+             'duration',
+             'campaign',
+             'pdays',
+             'previous',
+             'poutcome']
+
+TARGET = ['y']
+
+PIPELINE_NAME = "RF_Pipeline"
+PIPELINE_SAVE_FILE = f"{PIPELINE_NAME}_output_v"
+
+PACKAGE_ROOT = pathlib.Path(prediction_model.__file__).resolve().parent
+TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
+DATASET_DIR = PACKAGE_ROOT / "datasets"
---
+++ package/prediction_model/train_pipeline.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from package.prediction_model.config import config
+from package.prediction_model import pipeline
+from package.prediction_model.preprocessing.data_management import save_pipeline
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+from package.prediction_model.version import __version__ as _version
+
+def run_training():
+	data = pd.read_csv(config.TRAIN_DATASET_DIR)
+
+	X_train, y_train, X_test, y_test = train_test_split(data[config.FEATURES], data[config.TARGET], test_size=0.1,
+														random_state=0)
+
+	pipeline.prediction_pipe.fit(X_train, y_train)
+	_logger.info(f"saving model version: {_version}")
+	save_pipeline(pipeline_to_persist=pipeline.price_pipe)
+
+if __name__ == "__main__":
+	run_training()
